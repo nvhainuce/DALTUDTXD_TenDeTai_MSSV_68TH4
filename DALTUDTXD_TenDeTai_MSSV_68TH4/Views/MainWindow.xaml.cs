bb566033111a71c7b50038d74e31cab7c7a2b500
hainv1 <hainv1@huce.edu.vn>
2026-02-05 10:46:15 +0700

    Add project files.

---
+++ DALTUDTXD_TenDeTai_MSSV_68TH4/Views/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using DALTUDTXD_TenDeTai_MSSV_68TH4.Views.Pages;
+using DALTUDTXD_TenDeTai_MSSV_68TH4.Views.UserControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DALTUDTXD_TenDeTai_MSSV_68TH4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //khởi tạo UserControl Ribbon
+            UC_Ribbon uc_Ribbon = new UC_Ribbon(MainFrame);
+            Panel0.Children.Add(uc_Ribbon);
+            //load home page
+            MainFrame.Content = new HomePage();
+        }
+    }
+}
---
+++ DALTUDTXD_TenDeTai_MSSV_68TH4/Views/UserControls/UC_Ribbon.xaml.cs
@@ -0,0 +1,37 @@
+﻿using DALTUDTXD_TenDeTai_MSSV_68TH4.Views.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DALTUDTXD_TenDeTai_MSSV_68TH4.Views.UserControls
+{
+    /// <summary>
+    /// Interaction logic for UC_Ribbon.xaml
+    /// </summary>
+    public partial class UC_Ribbon : UserControl
+    {
+        //khai báo biến 
+        private Frame Mainframe;
+        public UC_Ribbon(Frame mainFrame)
+        {
+            InitializeComponent();
+            this.Mainframe = mainFrame;
+        }
+
+        private void rbt_2Dplan_Click(object sender, RoutedEventArgs e)
+        {
+            Mainframe.Content = new Plan2DPage();
+        }
+    }
+}
